ddcf56f08ffdc009c53306eb56e465920c03202d
NishanthSubramanian <nishanth80442@gmail.com>
2019-03-16 17:55:36 +0530

    Add events

--- app/src/main/java/com/example/hackathon/AddEventActivity.java
+++ app/src/main/java/com/example/hackathon/AddEventActivity.java
@@ -1,13 +1,80 @@
 package com.example.hackathon;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class AddEventActivity extends AppCompatActivity {
+    private TextInputLayout dateTime;
+    private TextInputEditText dateTimeEdit;
+    private String st = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+
+
+        dateTime = findViewById(R.id.date_time);
+        dateTimeEdit = findViewById(R.id.date_time_edit_text);
+        dateTimeEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectDateTime(v);
+                Log.d("BRO","CLICKED");
+            }
+        });
+
+    }
+
+
+    public void selectDateTime(View view) {
+        st = "";
+        final int mYear, mMonth, mDay, mHour, mMinute;
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(view.getContext(),
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                        st = st + year + "/" + monthOfYear + 1 + "/" + dayOfMonth;
+
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(view.getContext(),
+                                new TimePickerDialog.OnTimeSetListener() {
+                                    @Override
+                                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                        dateTimeEdit.setText(st + " " + hourOfDay + ":" + minute);
+                                        st = st + "/" + hourOfDay + "/" + minute;
+                                        Log.d("date time", st + "!");
+                                        //txtTime.setText(hourOfDay + ":" + minute);
+                                    }
+                                }, mHour, mMinute, false);
+
+                        timePickerDialog.show();
+
+                        //txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+        datePickerDialog.show();
+
     }
 }
